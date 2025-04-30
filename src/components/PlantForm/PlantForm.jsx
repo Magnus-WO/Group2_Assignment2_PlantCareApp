35e4efb64d84a7467f3e51ae2cddecd21149c548
Benedict Notoane <benni@MacBookPro.lan>
2025-04-30 22:59:42 +0200

    added validation on submit and clear error on change

--- src/components/PlantForm/PlantForm.jsx
+++ src/components/PlantForm/PlantForm.jsx
@@ -12,105 +12,98 @@ const PlantForm = ({ closeModal }) => {
     wateringSchedule: "",
     lightRequirement: "",
     soilType: "",
-
     temperatureMin: "",
     temperatureMax: "",
     humidity: "",
     imageUrl: "",
   });
   const [errorMessages, setErrorMessages] = useState({});
+  const [isValid, setIsValid] = useState(false);
 
-  const handleFieldValidation = (name, value) => {
+  const handleValidation = () => {
     const errors = { ...errorMessages };
-    let isValid = true;
+    let isFormValid = true;
 
     if (!formData.plantName.trim()) {
       errors.plantName = "Plant name is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.plantName = "";
     }
 
     if (!formData.toxicity.trim()) {
       errors.toxicity = "Toxicity is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.toxicity = "";
     }
 
     if (!formData.scientificName.trim()) {
       errors.scientificName = "Scientific name is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.scientificName = "";
     }
 
     if (!formData.wateringSchedule.trim()) {
       errors.wateringSchedule = "Watering schedule is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.wateringSchedule = "";
     }
 
     if (!formData.lightRequirement.trim()) {
       errors.lightRequirement = "Light requirement is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.lightRequirement = "";
     }
 
     if (!formData.soilType.trim()) {
       errors.soilType = "Soil type is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.soilType = "";
     }
 
     if (!formData.temperatureMin.trim() || isNaN(formData.temperatureMin)) {
       errors.temperatureMin = "Min temperature is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.temperatureMin = "";
     }
 
     if (!formData.temperatureMax.trim() || isNaN(formData.temperatureMax)) {
       errors.temperatureMax = "Max temperature is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.temperatureMax = "";
     }
 
     if (!formData.humidity.trim()) {
       errors.humidity = "Humidity is required!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.humidity = "";
     }
 
     if (!formData.imageUrl.trim()) {
       errors.imageUrl = "Image URL is required!";
-      isValid = false;
+      isFormValid = false;
     } else if (
       !/^https?:\/\/[\w.-]+\.[a-z]{2,}(\/[\w\-._~:/?#\[\]@!$&'()*+,;=%]*)*\.(jpg|jpeg|png|gif|webp)$/i.test(
-        value
+        formData.imageUrl
       )
     ) {
       errors.imageUrl = "Image URL must end with (jpg, jpeg, png, gif, webp)!";
-      isValid = false;
+      isFormValid = false;
     } else {
       errors.imageUrl = "";
     }
 
     setErrorMessages(errors);
-  };
-
-  const handleValidation = (e) => {
-    let isValid = true;
-    Object.entries(formData).forEach(([name, value]) => {
-      handleFieldValidation(name, value);
-      if (!value.trim()) isValid = false;
-    });
-    return isValid;
+    setIsValid(isFormValid);
+    return isFormValid;
   };
 
   const handleChange = (e) => {
@@ -119,17 +112,19 @@ const PlantForm = ({ closeModal }) => {
       ...prevData,
       [name]: value,
     }));
-  };
-
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
-    handleFieldValidation(name, value);
+    handleValidation();
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const isValid = handleValidation();
-    if (!isValid) return;
+    const isFormValid = handleValidation();
+    if (!isFormValid) {
+      console.log("Form was not submitted");
+      return;
+    } else {
+      console.log("Form was submitted successfully", formData);
+      closeModal();
+    }
   };
 
   return (
@@ -144,12 +139,10 @@ const PlantForm = ({ closeModal }) => {
             id="plantName"
             value={formData.plantName}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Golden bamboo"
           />
           {errorMessages && <ErrorMessage message={errorMessages.plantName} />}
         </div>
-        {/* -------------- */}
 
         <div className={styles.formGroup}>
           <label htmlFor="toxicity">Toxicity</label>
@@ -159,12 +152,11 @@ const PlantForm = ({ closeModal }) => {
             id="toxicity"
             value={formData.toxicity}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Non-toxic, Toxic to pets"
           />
           {errorMessages && <ErrorMessage message={errorMessages.toxicity} />}
         </div>
-        {/* -------------- */}
+
         <div className={styles.formGroup}>
           <label htmlFor="scientificName">Scientific name</label>
           <input
@@ -173,7 +165,6 @@ const PlantForm = ({ closeModal }) => {
             id="scientificName"
             value={formData.scientificName}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Phyllostachys aurea"
           />
           {errorMessages && (
@@ -189,14 +180,13 @@ const PlantForm = ({ closeModal }) => {
             id="wateringSchedule"
             value={formData.wateringSchedule}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Once a week"
           />
           {errorMessages && (
             <ErrorMessage message={errorMessages.wateringSchedule} />
           )}
         </div>
-        {/* -------------- */}
+
         <div className={styles.formGroup}>
           <label htmlFor="lightRequirement">Light requirement</label>
           <input
@@ -205,14 +195,13 @@ const PlantForm = ({ closeModal }) => {
             id="lightRequirement"
             value={formData.lightRequirement}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Full sun, Partial shade"
           />
           {errorMessages && (
             <ErrorMessage message={errorMessages.lightRequirement} />
           )}
         </div>
-        {/* -------------- */}
+
         <div className={styles.formGroup}>
           <label htmlFor="soilType">Soil type</label>
           <input
@@ -221,12 +210,11 @@ const PlantForm = ({ closeModal }) => {
             id="soilType"
             value={formData.soilType}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. Well-draining soil"
           />
           {errorMessages && <ErrorMessage message={errorMessages.soilType} />}
         </div>
-        {/* -------------- */}
+
         <div className={styles.formGroup}>
           <label htmlFor="temperatureRange">Temperature range</label>
           <div className={styles.temperatureInputs}>
@@ -237,10 +225,8 @@ const PlantForm = ({ closeModal }) => {
               id="temperatureMin"
               value={formData.temperatureMin}
               onChange={handleChange}
-              onBlur={handleBlur}
               placeholder="Min"
             />
-
             <span>–</span>
             <input
               className={styles.temperatureInput}
@@ -249,7 +235,6 @@ const PlantForm = ({ closeModal }) => {
               id="temperatureMax"
               value={formData.temperatureMax}
               onChange={handleChange}
-              onBlur={handleBlur}
               placeholder="Max"
             />
           </div>
@@ -257,13 +242,12 @@ const PlantForm = ({ closeModal }) => {
             {errorMessages && (
               <ErrorMessage message={errorMessages.temperatureMin} />
             )}
-
             {errorMessages && (
               <ErrorMessage message={errorMessages.temperatureMax} />
             )}
           </div>
         </div>
-        {/* -------------- */}
+
         <div className={styles.formGroup}>
           <label htmlFor="humidity">Humidity</label>
           <input
@@ -272,12 +256,10 @@ const PlantForm = ({ closeModal }) => {
             id="humidity"
             value={formData.humidity}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="e.g. High humidity"
           />
           {errorMessages && <ErrorMessage message={errorMessages.humidity} />}
         </div>
-        {/* -------------- */}
 
         <div className={styles.formGroup}>
           <label htmlFor="imageUrl">Image URL</label>
@@ -287,12 +269,10 @@ const PlantForm = ({ closeModal }) => {
             id="imageUrl"
             value={formData.imageUrl}
             onChange={handleChange}
-            onBlur={handleBlur}
             placeholder="https://example.com/plant.jpg"
           />
           {errorMessages && <ErrorMessage message={errorMessages.imageUrl} />}
         </div>
-        {/* -------------- */}
 
         <div className={styles.buttonContainer}>
           <Button type="submit" className={styles.formButton}>
